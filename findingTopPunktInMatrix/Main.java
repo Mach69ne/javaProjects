ec7dc8524a8a2cf0a539515706f2b2ab44f368cd
Elias Larsen <s235125@dtu.dk>
2024-02-01 14:48:58 +0100

    Added Main that runs algos

---
+++ findingTopPunktInMatrix/Main.java
@@ -0,0 +1,10 @@
+class Main 
+{
+    public static void main(String[] args)
+    {
+        var matrix = MatrixUtils.createMatrix();
+        System.out.println(Algos.startFromMiddle(matrix));
+
+        //System.out.println(Algos.findMaxInMiddle(matrix));
+    }
+}
