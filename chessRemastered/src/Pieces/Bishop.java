ebb379a63baee57d8232892f25bcc692d2ccec59
Elias Larsen <s235125@dtu.dk>
2024-02-29 00:41:04 +0100

    Added initial logic for bishop. It is still possible to move through enemy pieces.

--- chessRemastered/src/Pieces/Bishop.java
+++ chessRemastered/src/Pieces/Bishop.java
@@ -1,25 +1,59 @@
 package Pieces;
 
-import javafx.geometry.Pos;
-
-import javax.management.loading.ClassLoaderRepository;
-
 public class Bishop extends Piece
 {
     public Bishop(boolean isWhite, int x, int y)
     {
-        super(isWhite,x,y);
+        super(isWhite, x, y);
     }
 
     @Override
     public boolean checkIfMoveIsLegal(Position position) throws IllegalArgumentException
     {
-        return true;
+        if (this.getPosition().equals(position))
+        {
+            return true;
+        }
+        super.checkIfMoveIsLegal(position);
+
+        int deltaX = this.getPosition().x() - position.x();
+        int deltaY = this.getPosition().y() - position.y();
+        if (Math.abs(deltaX) < 1 && Math.abs(deltaY) < 1)
+        {
+            return true;
+        }
+        if (Math.abs(deltaX) != Math.abs(deltaY))
+        {
+            return false;
+        }
+
+        return checkIfPathIsClear(position);
     }
 
     @Override
     public char getSymbol()
     {
         return 'B';
+    }
+
+    private boolean checkIfPathIsClear(Position position)
+    {
+        if (getPosition().x() - position.x() > 0)
+        {
+            position = position.changeX(1);
+        }
+        else
+        {
+            position = position.changeX(-1);
+        }
+        if (getPosition().y() - position.y() > 0)
+        {
+            position = position.changeY(1);
+        }
+        else
+        {
+            position = position.changeY(-1);
+        }
+        return checkIfMoveIsLegal(position);
     }
 }
