a1b85be2c392d13907a4866b981278c2c87b7376
Elias Larsen <s235125@dtu.dk>
2024-02-29 00:33:20 +0100

    Added Equals and change methods for Position

--- chessRemastered/src/Pieces/Position.java
+++ chessRemastered/src/Pieces/Position.java
@@ -2,4 +2,18 @@ package Pieces;
 
 public record Position(int x, int y)
 {
+    public Position changeX(int x)
+    {
+        return new Position(this.x() + x, this.y());
+    }
+
+    public Position changeY(int y)
+    {
+        return new Position(this.x(), this.y() + y);
+    }
+
+    public boolean equals(Position position)
+    {
+        return this.x() == position.x() && this.y() == position.y();
+    }
 }
