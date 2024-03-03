0ef9614d61d212fb961f5cf9de4504c2b5ed1a0f
Elias Larsen <s235125@dtu.dk>
2024-03-03 23:16:48 +0100

    Added Queen

---
+++ chessRemastered/src/Pieces/Queen.java
@@ -0,0 +1,35 @@
+package Pieces;
+
+public class Queen extends Piece
+{
+    private final Rook rook;
+    private final Bishop bishop;
+
+    public Queen(boolean isWhite, int x, int y)
+    {
+        super(isWhite, x, y);
+        this.bishop = new Bishop(this.isWhite(), this.getPosition().x(), this.getPosition().y());
+        this.rook = new Rook(this.isWhite(), this.getPosition().x(), this.getPosition().y());
+        PieceManager.addPiece(this, x, y);
+    }
+
+    @Override
+    public boolean checkIfMoveIsLegal(Position position) throws IllegalArgumentException
+    {
+        super.checkIfMoveIsLegal(position);
+        boolean returnStatement = false;
+        rook.setPosition(this.getPosition());
+        bishop.setPosition(this.getPosition());
+        returnStatement |= rook.checkIfMoveIsLegal(position);
+        returnStatement |= bishop.checkIfMoveIsLegal(position);
+        this.setPosition(this.getPosition());
+        return returnStatement;
+
+    }
+
+    @Override
+    public char getSymbol()
+    {
+        return 'Q';
+    }
+}
