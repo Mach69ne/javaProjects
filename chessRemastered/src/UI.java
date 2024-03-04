5f065d25408f37544d005c32fbffee9d8d560460
Elias Larsen <s235125@dtu.dk>
2024-03-04 04:10:47 +0100

    The board is now rendered with pieces
    Signed-off-by: Elias Larsen <s235125@dtu.dk>

--- chessRemastered/src/UI.java
+++ chessRemastered/src/UI.java
@@ -1,9 +1,16 @@
+import Pieces.Piece;
+import Pieces.PieceManager;
+import Pieces.Position;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+// I know code quality in here sucks, but i stole it from the internet, so deal with it :)
+
 
 public class UI
 {
@@ -46,10 +53,63 @@ public class UI
                     }
                     white = !white;
                 }
+
+
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int k = 0; k < 8; k++)
+                    {
+                        int ind = 0;
+                        Piece piece = PieceManager.pieceOnSquare(new Position(i, k));
+                        if (piece == null)
+                        {
+                            continue;
+                        }
+
+                        switch (piece.getSymbol())
+                        {
+                            case 'K':
+                            {
+                                break;
+                            }
+                            case 'Q':
+                            {
+                                ind = 1;
+                                break;
+                            }
+                            case 'B':
+                            {
+                                ind = 2;
+                                break;
+                            }
+                            case 'N':
+                            {
+                                ind = 3;
+                                break;
+                            }
+                            case 'R':
+                            {
+                                ind = 4;
+                                break;
+                            }
+                            case 'P':
+                            {
+                                ind = 5;
+                                break;
+                            }
+                        }
+                        if (piece.isWhite())
+                        {
+                            ind += 6;
+                        }
+                        g.drawImage(imgs[ind], i * 64, k * 64, this);
+
+                    }
+                }
             }
         };
         frame.add(panel);
-        frame.setDefaultCloseOperation(3);
+        //frame.setDefaultCloseOperation.(WindowConstants.DO_NOTHING_ON_CLOSE);
         frame.setVisible(true);
     }
 }
