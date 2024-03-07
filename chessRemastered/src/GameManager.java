e21a5b6a1122383df01c5f02b2018d6f520421b0
Elias Larsen <s235125@dtu.dk>
2024-03-07 17:09:42 +0100

    Logic for castling
    Signed-off-by: Elias Larsen <s235125@dtu.dk>

--- chessRemastered/src/GameManager.java
+++ chessRemastered/src/GameManager.java
@@ -26,6 +26,10 @@ public class GameManager
 
     public static boolean isMoveLegal(Move move)
     {
+        if (moveIsCastle(move))
+        {
+            System.out.println(castle(move));
+        }
         Piece piece = move.piece();
         if (piece.isWhite() != whiteTurn)
         {
@@ -47,6 +51,59 @@ public class GameManager
             return isInCheck;
         }
         return false;
+    }
+
+    private static boolean moveIsCastle(Move move)
+    {
+        if (move.piece().isWhite() != whiteTurn)
+        {
+            return false;
+        }
+        if (move.piece().getSymbol() == 'K')
+        {
+            return Math.abs(move.toPos().x() - move.piece().getPosition().x()) == 2;
+        }
+        return false;
+    }
+
+    private static boolean castle(Move move)
+    {
+        Position rookPosition = null;
+        boolean kingSide = move.toPos().x() > 5;
+        if (PieceManager.isInCheck(whiteTurn))
+        {
+            return false;
+        }
+        if (kingSide)
+        {
+            rookPosition = new Position(7, move.toPos().y());
+        }
+        else
+        {
+            rookPosition = new Position(0, move.toPos().y());
+        }
+        Piece rook = PieceManager.pieceOnSquare(rookPosition);
+        Position positionToCheck = new Position(move.piece().getPosition().x(), move.piece().getPosition().y());
+        if (rook.getSymbol() != 'R')
+        {
+            return false;
+        }
+        boolean piecesBlocking = true;
+        if (kingSide)
+        {
+            if (isMoveLegal(new Move(rook, move.piece().getPosition().changeX(1))) && PieceManager.isEmpty(move.piece().getPosition().changeX(1)))
+            {
+                piecesBlocking = false;
+            }
+        }
+        else
+        {
+            if (isMoveLegal(new Move(rook, move.piece().getPosition().changeX(-1))) && PieceManager.isEmpty(move.piece().getPosition().changeX(-1)))
+            {
+                piecesBlocking = false;
+            }
+        }
+        return !piecesBlocking;
     }
 
     private static void resetMove(Move move, Piece pieceOnToSquare, Position oldSquare)
