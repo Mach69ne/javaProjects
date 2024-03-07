1e8ca86c1147f49c56b249753c9aa2030ec44f63
Elias Larsen <s235125@dtu.dk>
2024-03-07 15:43:02 +0100

    Now resets move if you would be in check after making a move
    Signed-off-by: Elias Larsen <s235125@dtu.dk>

--- chessRemastered/src/GameManager.java
+++ chessRemastered/src/GameManager.java
@@ -1,7 +1,10 @@
 import Pieces.Piece;
+import Pieces.PieceManager;
+import Pieces.Position;
 
 public class GameManager
 {
+    private static final boolean checkMate = false;
     private static boolean whiteTurn = true;
 
     public static boolean getWhiteTurn()
@@ -10,6 +13,18 @@ public class GameManager
     }
 
     public static boolean makeMove(Move move)
+    {
+        if (isMoveLegal(move))
+        {
+            move.piece().setPosition(move.toPos());
+            whiteTurn = !whiteTurn;
+            System.out.println(ChessEngine.calculatePosition());
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isMoveLegal(Move move)
     {
         Piece piece = move.piece();
         if (piece.isWhite() != whiteTurn)
@@ -22,12 +37,22 @@ public class GameManager
             {
                 return false;
             }
+            Piece pieceOnToSquare = PieceManager.pieceOnSquare(move.toPos());
+            Position oldPosition = piece.getPosition();
             piece.setPosition(move.toPos());
-            whiteTurn = !whiteTurn;
-            System.out.println(ChessEngine.calculatePosition());
-            return true;
+            boolean isInCheck = !PieceManager.isInCheck(whiteTurn);
+            resetMove(move, pieceOnToSquare, oldPosition);
+            return isInCheck;
         }
         return false;
     }
 
+    private static void resetMove(Move move, Piece pieceOnToSquare, Position oldSquare)
+    {
+        move.piece().setPosition(oldSquare);
+        if (pieceOnToSquare != null)
+        {
+            pieceOnToSquare.setPosition(move.toPos());
+        }
+    }
 }
