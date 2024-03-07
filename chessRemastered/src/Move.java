ccdbb853af2ba7858abc80b5cf507de18c8f815b
Elias Larsen <s235125@dtu.dk>
2024-03-07 02:57:10 +0100

    Ability to easily change variables in move
    Signed-off-by: Elias Larsen <s235125@dtu.dk>

--- chessRemastered/src/Move.java
+++ chessRemastered/src/Move.java
@@ -3,5 +3,13 @@ import Pieces.Position;
 
 public record Move(Piece piece, Position toPos)
 {
+    public Move changePos(Position position)
+    {
+        return new Move(piece, position);
+    }
 
+    public Move changePiece(Piece piece)
+    {
+        return new Move(piece, toPos);
+    }
 }
