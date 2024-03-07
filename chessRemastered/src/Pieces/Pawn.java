e5c398e87435836564459020a256d6c20b91b552
Elias Larsen <s235125@dtu.dk>
2024-03-07 03:37:53 +0100

    Pawns can no longer "jump" over another piece if they havent moved
    Signed-off-by: Elias Larsen <s235125@dtu.dk>

--- chessRemastered/src/Pieces/Pawn.java
+++ chessRemastered/src/Pieces/Pawn.java
@@ -18,11 +18,11 @@ public class Pawn extends Piece
         {
             if (this.getPosition().x() == position.x())
             {
-                if (isWhite() && position.y() == 3)
+                if (isWhite() && position.y() == 3 && PieceManager.isEmpty(new Position(position.x(), 2)))
                 {
                     return PieceManager.isEmpty(position);
                 }
-                else if (!isWhite() && position.y() == 4)
+                else if (!isWhite() && position.y() == 4 && PieceManager.isEmpty(new Position(position.x(), 5)))
                 {
                     return PieceManager.isEmpty(position);
                 }
