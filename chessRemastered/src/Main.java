fbf91373dd5172588ae946320a5e3a2d063ab2ee
Elias Larsen <s235125@dtu.dk>
2024-03-07 03:11:01 +0100

    Can now move pieces using ui instead of InputManager
    Signed-off-by: Elias Larsen <s235125@dtu.dk>

--- chessRemastered/src/Main.java
+++ chessRemastered/src/Main.java
@@ -33,45 +33,20 @@ public class Main
         while (true)
         {
             ui.update();
-
-
-            try
+            if (ui.getMove() == null || ui.getMove().piece() == null || ui.getMove().toPos() == null)
             {
-                if (ui.getMove().piece() == null || ui.getMove().toPos() == null || ui.getMove() == null)
-                {
-                    continue;
-                }
-                // Find piece on the working square, and try to move to target square
-                Piece piece = ui.getMove().piece();
-                if (piece == null)
-                {
-                    throw new IllegalArgumentException("That field is currently empty");
-                }
-                if (piece.isWhite() != whiteTurn)
-                {
-                    throw new IllegalArgumentException("You cannot move opponents piece");
-                }
-                if (piece.checkIfMoveIsLegal(ui.getMove().toPos()))
-                {
-                    piece.setPosition(ui.getMove().toPos());
-                    System.out.println("Moved piece");
-                }
-                else
-                {
-                    System.out.println("That is not a legal move");
-                }
-
-            }
-            catch (IllegalArgumentException e)
-            {
-                System.out.println(e.getMessage());
                 continue;
             }
-            catch (Exception e)
+            // Find piece on the working square, and try to move to target square
+            Piece piece = ui.getMove().piece();
+            if (piece.isWhite() != whiteTurn)
             {
-                PieceManager.resetBoard();
-                whiteTurn = true;
                 continue;
+            }
+            if (piece.checkIfMoveIsLegal(ui.getMove().toPos()))
+            {
+                piece.setPosition(ui.getMove().toPos());
+                whiteTurn = !whiteTurn;
             }
             /*
             boolean inCheck = PieceManager.isInCheck(whiteTurn);
@@ -81,7 +56,7 @@ public class Main
                             continue;
                         }
             */
-            whiteTurn = !whiteTurn;
+
             //System.out.println(ChessEngine.calculatePosition());
             //PieceManager.printBoard();
 
